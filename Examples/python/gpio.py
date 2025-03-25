3e2cf4c556089718cd40c3abf9d9cacb216ce2f5
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-25 14:28:00 +0100

    GPIO try #6

--- Examples/python/gpio.py
+++ Examples/python/gpio.py
@@ -9,20 +9,20 @@ rp.rp_Init()
 # Set all DIO_P pins to be outputs
 rp.rp_GPIOpSetDirection(0b00001000)
 # Set all DIO_N pins to be outputs
-rp.rp_GPIOnSetDirection(0b00001000)
+# rp.rp_GPIOnSetDirection(0b00001000)
 
 
 print(f"Column P directions: {int(rp.rp_GPIOpGetDirection()[1]) & 0b11111111:>08b}")
-print(f"Column N directions: {int(rp.rp_GPIOnGetDirection()[1]) & 0b11111111:>08b}")
+# print(f"Column N directions: {int(rp.rp_GPIOnGetDirection()[1]) & 0b11111111:>08b}")
 
 for i in range(10):
     print("Setting the ON state")
     rp.rp_GPIOpSetState(0b00001000)
-    rp.rp_GPIOnSetState(0b00001000)
+    # rp.rp_GPIOnSetState(0b00001000)
     time.sleep(1)
     print("Setting the OFF state")
     rp.rp_GPIOpSetState(0b00000000)
-    rp.rp_GPIOnSetState(0b00000000)
+    # rp.rp_GPIOnSetState(0b00000000)
     time.sleep(1)
 
 rp.rp_Release()
