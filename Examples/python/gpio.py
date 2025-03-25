b0fe3766b21e354fd0b2045fe2252942296756fe
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-25 13:25:03 +0100

    Updated set state

--- Examples/python/gpio.py
+++ Examples/python/gpio.py
@@ -17,12 +17,12 @@ print(f"Column N directions: {int(rp.rp_GPIOnGetDirection()[1]) & 0b11111111:>08
 
 for i in range(10):
     print("Setting the ON state")
-    rp.rp_GPIOpSetState(0b11111111)
-    rp.rp_GPIOnSetState(0b11111111)
+    rp.rp_GPIOpSetState(0b00000110)
+    rp.rp_GPIOnSetState(0b00000110)
     time.sleep(1)
     print("Setting the OFF state")
-    rp.rp_GPIOpSetState(0b00000000)
-    rp.rp_GPIOnSetState(0b00000000)
+    rp.rp_GPIOpSetState(0b00000110)
+    rp.rp_GPIOnSetState(0b00000110)
     time.sleep(1)
 
 rp.rp_Release()
