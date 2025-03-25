33b027ee525421617a1416bc320b81c0e7796cc0
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-25 12:10:00 +0100

    Tets on all DIO pins

--- Examples/python/gpio.py
+++ Examples/python/gpio.py
@@ -5,16 +5,23 @@ import rp
 fpga = overlay()
 rp.rp_Init()
 
+# Set all DIO_P pins to be outputs
 rp.rp_GPIOpSetDirection(0b11111111)
-print(f"Column P directions: {rp.rp_GPIOpGetDirection()[1]:>08b}")
-print(f"Column N directions: {rp.rp_GPIOpGetDirection()[0]:>08b}")
+# Set all DIO_N pins to be outputs
+rp.rp_GPIOnSetDirection(0b11111111)
+
+
+print(f"Column P directions: {int(rp.rp_GPIOpGetDirection()[1]) & 0b11111111:>08b}")
+print(f"Column N directions: {int(rp.rp_GPIOnGetDirection()[1]) & 0b11111111:>08b}")
 
 for i in range(10):
     print("Setting the ON state")
     rp.rp_GPIOpSetState(0b11111111)
+    rp.rp_GPIOnSetState(0b11111111)
     time.sleep(1)
     print("Setting the OFF state")
     rp.rp_GPIOpSetState(0b00000000)
+    rp.rp_GPIOnSetState(0b00000000)
     time.sleep(1)
 
 rp.rp_Release()
