2442384e74eb77d5e9635c64438174d926e37015
ivan.ruzavin <ivan.ruzavin@mikroe.com>
2025-03-27 08:13:45 +0100

    Brushless try #5

--- Examples/python/brushless_17.py
+++ Examples/python/brushless_17.py
@@ -23,14 +23,14 @@ rp.rp_GPIOpSetDirection(0b00011000)
 def set_motor_mode(motor_mode):
     # Counter ClockWise direction
     if motor_mode == "MODE_CCW":
-        # Set CS to LOW and RST to HIGH
+        # Set RST to LOW and CS to HIGH
         rp.rp_GPIOnSetState(0b00010000)
         # Set INT to HIGH
         rp.rp_GPIOpSetState(0b00010000)
     # ClockWise direction
     elif motor_mode == "MODE_CW":
-        # Set CS to HIGH and RST to LOW
-        rp.rp_GPIOnSetState(0b00000000)
+        # Set RST to HIGH and CS to HIGH
+        rp.rp_GPIOnSetState(0b00011000)
         # Set INT to HIGH
         rp.rp_GPIOpSetState(0b00010000)
     elif motor_mode == "MODE_STOP":
